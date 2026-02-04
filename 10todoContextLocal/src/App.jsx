0ebf7ca1d91bff659e6ced734fdd6e9831511e09
kodejeet <roshansahx2003@gmail.com>
2026-02-04 20:50:45 +0530

    debug changes

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,50 +1,61 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import { TodoProvider } from "./contexts";
+import { TodoForm, TodoItem } from "./components/";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo)
-  }
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
   const updateTodo = (id, todo) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === ? todo : prevTodo )))
-  }
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
+    );
+  };
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id))
-  }
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
-  const toggleComplete = (id) => { 
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prevTodo, completed : !prevTodo.completed } : prevTodo ) )
-  }
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo,
+      ),
+    );
+  };
 
   useEffect(() => {
-    const todos = JSON.parse(localStorage.getItems("todos"))
+    const todos = JSON.parse(localStorage.getItem("todos"));
 
-    if (todos && todos.length > 0 ) {
-      setTodos(todos)  
-  },[])
-  
-  
-  useEffect(() => {
-    
-  
-    return () => {
-    const todos = JSON.parse(localStorage.setItems("todos", JSON.stringify(todos)))
+    if (todos && todos.length > 0) {
+      setTodos(todos);
     }
-  },[todos])
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <TodoProvider value = {{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
           Manage Your Todos
         </h1>
-        <div className="mb-4">{/* Todo form goes here */}</div>
+        <div className="mb-4">
+          <TodoForm />
+        </div>
         <div className="flex flex-wrap gap-y-3">
-          {/*Loop and Add TodoItem here */}
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))}
         </div>
       </div>
     </TodoProvider>
--- 10todoContextLocal/src/contexts/TodosContext.js
+++ 10todoContextLocal/src/contexts/TodosContext.js
@@ -4,7 +4,7 @@ export const TodoContext = createContext({
   todos: [
     {
       id: 1,
-      todo: "Todo Msg", completed: "false", },
+      todo: "Todo Msg", completed: false, },
   ],
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
