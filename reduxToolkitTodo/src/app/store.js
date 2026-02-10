0d3574606be8995ebd163cd9902c1625823f2c5f
kodejeet <roshansahx2003@gmail.com>
2026-02-10 23:39:16 +0530

    add: todo components

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -1,3 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import TodoReducer from "../features/todo/todoSlice.js";
 
-export const store = configureStore({});
+export const store = configureStore({
+    reducer: todoReduce
+  
+});
